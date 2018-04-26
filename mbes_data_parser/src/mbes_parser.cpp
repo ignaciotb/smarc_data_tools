8a40112b55b2ea37e5ff1c589344348667c7ceab
Nacho <torroba@kth.se>
2018-04-26 10:13:34 +0200

    Parsing space-separated numbers from all files under given folder

--- mbes_data_parser/src/mbes_parser.cpp
+++ mbes_data_parser/src/mbes_parser.cpp
@@ -3,34 +3,61 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <dirent.h>
 
 class MBESParser{
 
 public:
     MBESParser(std::string node_name, ros::NodeHandle &nh):node_name_(node_name), nh_(&nh) {
 
-        std::ifstream infile("/home/nacho/catkin_ws/src/smarc-project/smarc_data_tools/mbes_data_parser/Data/NavUTM/170909022939_UserDefined_copy.nav");
-        if (infile.is_open()) {
-            ROS_INFO_STREAM(node_name_ << ": file open");
+        // Open directory and check all files inside
+        DIR *dir;
+        if ((dir = opendir ("/home/nacho/catkin_ws/src/smarc-project/smarc_data_tools/mbes_data_parser/Data/NavUTM")) != NULL) {
+            std::ifstream infile;
+            std::string delimiter = " ";
+            std::string line;
+
+            // Check files and directories within directory
+            struct dirent *ent;
+            while ((ent = readdir (dir)) != NULL) {
+                if( ent->d_type == DT_DIR ){
+                    // If directory, move on
+                    continue;
+                }
+
+                // Open file and clean up line
+                infile.open(ent->d_name);
+                line.clear();
+                if (infile.is_open()) {
+                    ROS_INFO_STREAM(node_name_ << ": file open");
+                }
+                else{
+                    ROS_ERROR("Could not open file");
+                    continue;
+                }
+
+                // Read each line
+                while (std::getline(infile, line)){
+                    // Extract space-separated numbers
+                    while(true){
+                        if(line.find(delimiter) == -1){
+                            std::cout << line << std::endl;
+                            std::cout << std::endl;
+                            break;
+                        }
+                        std::cout << line.substr(0, line.find(delimiter)) << std::endl;
+                        line = line.substr(line.find(delimiter) + 1, line.size());
+                    }
+                }
+                infile.close();
+            }
+            closedir (dir);
         }
         else{
-            ROS_ERROR("Could not open file");
-            return;
+            /* could not open directory */
+            ROS_ERROR("Could not check directory");
         }
-
-        std::string delimiter = " ";
-        std::string line;
-        while (std::getline(infile, line)){
-            while(true){
-                if(line.find(delimiter) == -1){
-                    std::cout << line << std::endl;
-                    break;
-                }
-                std::cout << line.substr(0, line.find(delimiter)) << std::endl;
-                line = line.substr(line.find(delimiter) + 1, line.size());
-            }
-        }
-        ROS_INFO_STREAM(node_name_ << ", finished");
+        ROS_INFO_STREAM(node_name_ << ", finished reading files");
     }
 
 

ef8f4e21141667f0aed060203f3192b620a7e485
Nacho <torroba@kth.se>
2018-04-26 14:37:30 +0200

    ROV tracks in RVIZ

--- mbes_data_parser/src/mbes_parser.cpp
+++ mbes_data_parser/src/mbes_parser.cpp
@@ -1,5 +1,7 @@
 
 #include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
+#include <visualization_msgs/MarkerArray.h>
 
 #include <fstream>
 #include <sstream>
@@ -21,10 +23,51 @@ public:
         const char* dir_name = "/home/nacho/catkin_ws/src/smarc-project/smarc_data_tools/mbes_data_parser/Data/NavUTM/";
         readNavFilesInDir(dir_name);
 
+        track_pub_ = nh_->advertise<visualization_msgs::MarkerArray>((node_name_ + "/rov_track"), 10);
+        map_frame_ = "map";
+
+        while(ros::ok()){
+            updateMapMarkers(1);
+            ros::Duration(10).sleep();
+        }
+
     }
 
 
 private:
+
+    void updateMapMarkers(double color){
+
+        int cnt = 0;
+        visualization_msgs::MarkerArray marker_array;
+        visualization_msgs::Marker marker;
+        for(const std::tuple<double, double, double, double> pose_t: rov_coord_){
+            marker.header.frame_id = map_frame_;
+            marker.header.stamp = ros::Time::now();//ros::Time::fromSec(std::get<0>(pose_t)); ****
+            marker.ns = "rov_track";
+            marker.id = ++cnt;
+            marker.type = visualization_msgs::Marker::SPHERE;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = std::get<1>(pose_t);
+            marker.pose.position.y = std::get<2>(pose_t);
+            marker.pose.position.z = std::get<3>(pose_t);
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.x = 0.1;
+            marker.scale.y = 0.1;
+            marker.scale.z = 0.1;
+            marker.color.a = 1.0;
+            marker.color.r = 1.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
+
+            marker_array.markers.push_back(marker);
+        }
+        track_pub_.publish(marker_array);
+    }
+
 
     void readNavFilesInDir(auto dir_path){
         // Open directory and check all files inside
@@ -41,22 +84,30 @@ private:
 
             // Check files and directories within directory
             struct dirent *ent;
+            std::vector<std::string> files;
             while ((ent = readdir (dir)) != NULL) {
-                if( ent->d_type == DT_DIR ){
+                if( ent->d_type != DT_DIR ){
                     // If directory, move on
-                    continue;
+                    files.push_back(std::string(ent->d_name));
                 }
+            }
+            closedir(dir);
 
-                // Open file and clean up line
-                char buffer[256]; // <- danger, only storage for 256 characters.
-                strncpy(buffer, dir_path, sizeof(buffer));
-                strncat(buffer, ent->d_name, sizeof(buffer));
+            // Sort the files before parsing them (naming == acquisition time)
+            std::sort(files.begin(), files.end());
+            std::for_each(files.begin(), files.end(), [](std::string f){
+               std::cout << f << std::endl;
+            });
 
-                std::cout << buffer << std::endl;
-                infile.open(buffer);
+            // For every file in the dir
+            for(const std::string file: files){
+
+                // Open file
+                std::cout << std::string(dir_path) + file << std::endl;
+                infile.open(std::string(dir_path) + file);
                 line.clear();
-                if (infile.is_open()) {
-                    ROS_INFO_STREAM(node_name_ << ": file open");
+                if(infile.is_open()) {
+                    ROS_DEBUG_STREAM(node_name_ << ": file open");
                 }
                 else{
                     ROS_ERROR("Could not open file");
@@ -65,14 +116,11 @@ private:
 
                 // Read each line
                 while (std::getline(infile, line)){
-                    if(first_pose_ == true){
-                        first_pose_ = false;
-                    }
                     pose_in_line = 0;
                     while(true){
                         if(line.find(spc_delimiter) == -1){
-                            // case 5:
-                            std::cout << std::endl;
+                            // case 5: exit
+//                            std::cout << std::endl;
                             break;
                         }
 
@@ -81,7 +129,7 @@ private:
                         // 1: Time
                         // 2: Easting
                         // 3: Northing
-                        // 4: Depth (positive values!)
+                        // 4: Depth (given in positive values!)
                         // 5: Zeros
                         std::string time_str;
                         switch(pose_in_line){
@@ -98,24 +146,25 @@ private:
                                 northing = std::stod(line.substr(0, line.find(spc_delimiter)));
                                 break;
                             case 4:
-                                depth = std::stod(line.substr(0, line.find(spc_delimiter)));
+                                depth = -1 * std::stod(line.substr(0, line.find(spc_delimiter)));
                                 break;
                         }
 
-                        std::cout << std::stod(line.substr(0, line.find(spc_delimiter))) << std::endl;
                         line = line.substr(line.find(spc_delimiter) + 1, line.size());
                         pose_in_line += 1;
-
                     }
+
+                    // Initialize origin with first input
                     if(first_pose_ == true){
                         first_pose_ = false;
                         time_stamp_origin = time_stamp;
                         easting_origin = easting;
                         northing_origin = northing;
                         depth_origin = 0;
-
                     }
+
 //                    std::cout << "Line content: " << time_stamp << ", " << easting << ", " << northing << ", " << depth << std::endl;
+//                    std::cout << "Line content: " << time_stamp - time_stamp_origin << ", " << easting - easting_origin << ", " << northing - northing_origin << ", " << depth - depth_origin << std::endl;
                     rov_coord_.emplace_back(time_stamp - time_stamp_origin,
                                             easting - easting_origin,
                                             northing - northing_origin,
@@ -123,7 +172,6 @@ private:
                 }
                 infile.close();
             }
-            closedir (dir);
         }
         else{
             // Could not open directory
@@ -165,11 +213,12 @@ private:
         return time_stamp;
     }
 
-
-
-    bool first_pose_;
     std::string node_name_;
     ros::NodeHandle *nh_;
+    std::string map_frame_;
+    ros::Publisher track_pub_;
+
+    bool first_pose_;
     std::vector<std::tuple<double, double, double, double>> rov_coord_;
 
 };
@@ -180,7 +229,7 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
     boost::shared_ptr<MBESParser> mbes_parser(new MBESParser(ros::this_node::getName(), nh));
 
-//    ros::waitForShutdown();
+    ros::waitForShutdown();
 
 //    if(!ros::ok()){
         mbes_parser.reset();
